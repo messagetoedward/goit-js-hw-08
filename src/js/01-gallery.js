8a8a00e49167cde9017fbca7193208cddfacb795
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-10-31 23:14:20 +0200

    light box done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,24 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(SimpleLightbox);
+
+const gallery = galleryItems;
+console.log(gallery);
+const ulGallery = document.querySelector('.gallery');
+console.log(ulGallery);
+
+const markup = gallery.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+}).join('');
+
+ulGallery.insertAdjacentHTML('beforeend', markup);
+
+const lighbox = new SimpleLightbox('.gallery a', { 'captionsData': 'alt', 'captionDelay': '250' });
+lighbox.on('show.simplelightbox');
