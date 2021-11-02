8a9f9c67c6593a5a9ea71a86c43ddb130b60dec9
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-11-03 00:46:19 +0200

    task 2, done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import throttle from "lodash.throttle";
+const iframe = document.querySelector("#vimeo-player")
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+resumeTime();
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, data.seconds);
+}, 1000));
+
+function resumeTime() {
+    let setTime = localStorage.getItem(LOCALSTORAGE_KEY);
+    player.setCurrentTime(setTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+    console.log(setTime);
+}
 
