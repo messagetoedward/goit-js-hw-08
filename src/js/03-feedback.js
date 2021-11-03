3ceb153f6ff1a79719a509c274468793ba054c03
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-11-04 01:00:14 +0200

    3 task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
+const form = document.querySelector('form');
+const email = document.querySelector('input');
+const message = document.querySelector('textArea');
+const feedbackFormState = {};
+console.log(form);
 
+inputData();
+
+const formInput = (event) => {
+    feedbackFormState[event.target.name] = event.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState))
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const elements = { email: email.value, message: message.value }
+    console.log(elements);
+    email.value = "";
+    message.value = "";
+    localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', throttle((formInput), 500));
+form.addEventListener('submit', handleSubmit);
+
+function inputData() {
+    let existingData = localStorage.getItem('feedback-form-state');
+    console.log(existingData);
+    if (existingData) {
+        existingData = JSON.parse(existingData);
+        Object.entries(existingData).forEach(([name, value]) => {
+            form.elements[name].value = value;
+        })
+    } else { email.value = "", message.value = ""; };
+}
